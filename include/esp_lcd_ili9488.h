3b3989a70831ceceb309e6129bbd39213c9c9d85
Dirk Thieme <dthieme@camposengineering.com>
2026-04-20 16:07:57 -0500

    added support for IPS display, based on LovyanGFX ILI9488 IPS code

--- include/esp_lcd_ili9488.h
+++ include/esp_lcd_ili9488.h
@@ -30,12 +30,28 @@ extern "C" {
  * 
  * NOTE: For parallel IO (Intel 8080) interface 16-bit color mode should
  * be used and @param buffer_size will be ignored.
-
  */
 esp_err_t esp_lcd_new_panel_ili9488(const esp_lcd_panel_io_handle_t io,
                                     const esp_lcd_panel_dev_config_t *panel_dev_config,
                                     const size_t buffer_size,
                                     esp_lcd_panel_handle_t *ret_panel);
+
+/**
+ * @brief Create LCD panel for model ILI9488 using IPS-specific initialization
+ *
+ * @param[in] io LCD panel IO handle
+ * @param[in] panel_dev_config general panel device configuration
+ * @param[in] buffer_size size of buffer to allocate for color conversions.
+ * @param[out] ret_panel Returned LCD panel handle
+ * @return
+ *          - ESP_ERR_INVALID_ARG   if parameter is invalid
+ *          - ESP_ERR_NO_MEM        if out of memory
+ *          - ESP_OK                on success
+ */
+esp_err_t esp_lcd_new_panel_ili9488_ips(const esp_lcd_panel_io_handle_t io,
+                                        const esp_lcd_panel_dev_config_t *panel_dev_config,
+                                        const size_t buffer_size,
+                                        esp_lcd_panel_handle_t *ret_panel);
 
 #ifdef __cplusplus
 }
